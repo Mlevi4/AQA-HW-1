493e40b109c8af922df730e78a83b9b588284eec
Mike Lebedev <to-mike@ya.ru>
2023-09-06 18:46:50 +0300

    Junit4 + testfail

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,13 +1,13 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
 
     @Test
-    public void testRemain() {
+    public void testWhen400ToAdd() {
 
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 600;
@@ -15,6 +15,31 @@ public class CashbackHackServiceTest {
         int actual = cashbackHackService.remain(amount);
         int expected = 400;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
+
+    @Test
+    public void testWhenNothingToAdd() {
+
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testWhen1000ToAdd() {
+
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 0;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1000;
+
+        assertEquals(expected, actual);
+    }
+
 }
