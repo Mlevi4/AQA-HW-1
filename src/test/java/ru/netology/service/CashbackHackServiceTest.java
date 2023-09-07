4f80ad4fb902c8a46edc29c169396e0ef2d9949b
Mike Lebedev <to-mike@ya.ru>
2023-09-07 15:05:53 +0300

    testfall added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,13 +7,37 @@ import static org.testng.Assert.*;
 public class CashbackHackServiceTest {
 
     @Test
-    public void testRemain() {
+    public void testWhen400ToAdd() {
 
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 600;
 
         int actual = cashbackHackService.remain(amount);
         int expected = 400;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testWhen1000ToAdd() {
+
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 0;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testWhenNothingToAdd() {
+
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
 
         assertEquals(actual, expected);
     }
